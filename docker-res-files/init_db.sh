1cc5239b520745b3d9057cc83700a96106106ee0
jvinaches <jvinaches@naevatec.com>
2017-01-23 15:44:08 +0100

    Add Naevatec files

--- docker-res-files/init_db.sh
+++ docker-res-files/init_db.sh
@@ -2,3 +2,9 @@
 /usr/bin/mysqld_safe &
 sleep 5
 mysql -u root -e "CREATE DATABASE kagenda"
+sleep 5 
+mysql -u root -e "CREATE USER 'khc'@'localhost' IDENTIFIED BY 'khc'"
+sleep 5
+mysql -u root -e "GRANT ALL PRIVILEGES ON kagenda.* TO 'khc'@'localhost'"
+sleep 5
+mysql -u root -e "FLUSH PRIVILEGES"
